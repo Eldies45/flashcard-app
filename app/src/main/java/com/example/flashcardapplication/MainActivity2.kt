d5ae92384d571e8b49dec9a0f979b75c61aa570f
Eldies Tshiseke <st10468232@vcconnect.edu.za>
2025-04-28 17:05:49 +0200

    Flashcard app

--- app/src/main/java/com/example/flashcardapplication/MainActivity2.kt
+++ app/src/main/java/com/example/flashcardapplication/MainActivity2.kt
@@ -1,20 +1,35 @@
 package com.example.flashcardapplication
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity2 : AppCompatActivity() {
+    private val questions = arrayOf(
+        "Madame C.G. walker was the first american’s black millionaire.",
+        "The French sculptor Frédéric Auguste created the statue of liberty.",
+        "The Berlin Wall fell in 1975.",
+        "Julius Caesar was a Roman emperor.",
+        "The Titanic sank in 1912."
+    )
+
+    private val answers = arrayOf(true, true, false, false, true)
+
+    private var currentQuestionIndex = 0
+    private var score = 0
+
+    private lateinit var txtQuestion: TextView
+    private lateinit var btnTrue: Button
+    private lateinit var btnFalse: Button
+    private lateinit var btnNext: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main2)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
         }
     }
-}
